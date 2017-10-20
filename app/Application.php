11c6dcfaf1664f03e481c5904816519508ee6d4b
Nabil001 <nabil.lemenuel@gmx.fr>
2017-10-20 17:03:37 +0200

    Modified the 'run()' method

--- app/Application.php
+++ app/Application.php
@@ -12,21 +12,25 @@ class Application {
 
     public function run() {
         $router = new \Library\Router($this);
-        $router->addRoute(new Route('Default', 'home', '#^(/home)?/$#'));
-        $router->addRoute(new Route('Post', 'add', '#^/add/?$#'));
-        $router->addRoute(new Route('Post', 'list', '#^/list/([1-9]{1,})/?$#'));
-        $router->addRoute(new Route('Post', 'show', '#^/show-([1-9][0-9]{0,})/?$#', array('id')));
-        $router->addRoute(new Route('Post', 'update', '#^/update-([1-9][0-9]{0,})/?$#', array('id')));
+        $router->addRoute(new \Library\Route('Default', 'home', '#^/(home)?/?$#'));
+        $router->addRoute(new \Library\Route('Post', 'add', '#^/add/?$#'));
+        $router->addRoute(new \Library\Route('Post', 'list', '#^/list/([1-9]{1,})/?$#'));
+        $router->addRoute(new \Library\Route('Post', 'show', '#^/show-([1-9][0-9]{0,})/?$#', array('id')));
+        $router->addRoute(new \Library\Route('Post', 'update', '#^/update-([1-9][0-9]{0,})/?$#', array('id')));
 
-        $controller = $router->getController();
-
+        try {
+            $controller = $router->getController();
+        } catch (\Library\Exceptions\NoSuchRouteException $e) {
+            $controller = $this->get404ErrorController();
+        }
+        
         $response = $controller->execute();
         $response->send();
     }
 
     public function get404ErrorController() {
         //header($this->request->getProtocol() . '404 Not Found', true);
-        $controller = new Error\ErrorController(
+        $controller = new Controllers\ErrorController(
                                                 new Route('Default', 'show404', ''),
                                                 $this->request,
                                                 $this
@@ -35,7 +39,7 @@ class Application {
 
     public function get503ErrorController() {
         //header($this->request->getProtocol() . '503 Service Unavailable', true);
-        $controller = new Error\ErrorController(
+        $controller = new Controllers\ErrorController(
                                                 new Route('Default', 'show503', ''),
                                                 $this->request,
                                                 $this
