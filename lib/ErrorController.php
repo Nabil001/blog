665b142a35355401e1712e1d61844eda8edbb150
Nabil001 <nabil.lemenuel@gmx.fr>
2017-10-23 17:29:48 +0200

    Completed ErrorController class actions

--- lib/ErrorController.php
+++ lib/ErrorController.php
@@ -5,6 +5,14 @@ namespace Library;
 class ErrorController extends \Library\Controller {
 
     public function show404Action() {
+        header($this->application->getRequest()->getProtocol() . '404 Not Found', true);
+
+        return $this->template->render();
+    }
+
+    public function show503Action() {
+        header($this->application->getRequest()->getProtocol() . '503 Service Unavailable', true);
+
         return $this->template->render();
     }
 
