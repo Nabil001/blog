fdd4f830ceaaad7d3a1c09a654ee64d7e9a89b8d
Nabil001 <nabil.lemenuel@gmx.fr>
2017-10-31 13:57:45 +0100

    Corrections on ErrorController

--- lib/ErrorController.php
+++ lib/ErrorController.php
@@ -7,13 +7,13 @@ class ErrorController extends \Library\Controller {
     public function show404Action() {
         header($this->application->getRequest()->getProtocol() . '404 Not Found', true);
 
-        return $this->render($this->route->getAction());
+        return $this->render('show404');
     }
 
     public function show503Action() {
         header($this->application->getRequest()->getProtocol() . '503 Service Unavailable', true);
 
-        return $this->render($this->route->getAction());
+        return $this->render('show503');
     }
 
 }
